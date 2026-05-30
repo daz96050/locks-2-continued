755bf38cb96ed3ccc9261e8e6b6a34ab0e1be003
DAZ96050 <daz96050.gitub@gmail.com>
2026-05-30 03:08:43 -0500

    Update for 1.6

---
+++ Source/Locks2/Harmony/Optimizations/PathFinder_Patch.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using HarmonyLib;
+using Locks2.Core;
+using Verse;
+using Verse.AI;
+
+namespace Locks2.Harmony
+{
+    [HarmonyPatch(typeof(PathFinder), nameof(PathFinder.FindPathNow), typeof(IntVec3), typeof(LocalTargetInfo),
+        typeof(TraverseParms), typeof(PathFinderCostTuning?), typeof(PathEndMode), typeof(PathRequest.IPathGridCustomizer))]
+    public class PathFinder_FindPath_Patch
+    {
+        private const int MAX_FAILS = 3;
+        private static readonly Dictionary<int, Pair<int, int>> cache = new Dictionary<int, Pair<int, int>>();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int GetKey(TraverseParms traverseParms, LocalTargetInfo target)
+        {
+            int hash;
+            unchecked
+            {
+                hash = traverseParms.pawn?.thingIDNumber.GetHashCode() ?? 0;
+                hash = hash ^ (target.Cell.GetHashCode() << 1);
+            }
+            return hash;
+        }
+
+        public static void Postfix(PawnPath __result, TraverseParms traverseParms, LocalTargetInfo target)
+        {
+            if (__result != PawnPath.NotFound) return;
+            if (traverseParms.pawn == null) return;
+            var key = GetKey(traverseParms, target);
+            if (cache.TryGetValue(key, out var store) && GenTicks.TicksGame - store.second < 2500)
+            {
+                if (store.first > MAX_FAILS)
+                {
+                    cache.Remove(key);
+                    traverseParms.pawn.Map?.reachability?.cache?.ClearFor(traverseParms.pawn);
+                    LockConfig.Notify_Dirty();
+                }
+                else
+                {
+                    store.first += 1;
+                    store.second = GenTicks.TicksGame;
+                    cache[key] = store;
+                }
+
+                return;
+            }
+            cache[key] = new Pair<int, int>(1, GenTicks.TicksGame);
+        }
+    }
+}
---
+++ Source/Locks2/Harmony/Pawn_Ownership_Patch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using Locks2.Core;
+using RimWorld;
+
+namespace Locks2.Harmony
+{
+    [HarmonyPatch(typeof(Pawn_Ownership), nameof(Pawn_Ownership.ClaimBedIfNonMedical))]
+    public class Pawn_Ownership_ClaimBedIfNonMedical_Patch
+    {
+        public static void Prefix(Pawn_Ownership __instance)
+        {
+            __instance?.pawn?.Map?.reachability?.ClearCache();
+            LockConfig.Notify_Dirty();
+        }
+    }
+
+    [HarmonyPatch(typeof(Pawn_Ownership), nameof(Pawn_Ownership.UnclaimBed))]
+    public class Pawn_Ownership_UnclaimBed_Patch
+    {
+        public static void Prefix(Pawn_Ownership __instance)
+        {
+            __instance?.pawn?.Map?.reachability?.ClearCache();
+            LockConfig.Notify_Dirty();
+        }
+    }
+}
